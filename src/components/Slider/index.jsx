8df569c49f78a1491c51558db53a999c9ed191c9
Luis Gustavo Braga <luisgustavobsilva@gmail.com>
2024-01-09 11:42:00 -0300

    Criando componente slider e algumas configurações para esse componente

---
+++ src/components/Slider/index.jsx
@@ -0,0 +1,17 @@
+import '../../styles/components/slider.sass';
+
+import { register } from 'swiper/element/bundle';
+register();
+import 'swiper/element/css/autoplay';
+
+export default function Slider() {
+
+    return (
+
+        <section>
+
+
+        </section>
+
+    )
+}
