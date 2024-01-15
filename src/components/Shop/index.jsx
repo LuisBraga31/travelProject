9da8bb215f8f8a15935606afeec75da79ff068b9
Luis Gustavo Braga <luisgustavobsilva@gmail.com>
2024-01-14 21:12:11 -0300

    Estilizando componente 'Shop'

--- src/components/Shop/index.jsx
+++ src/components/Shop/index.jsx
@@ -1,11 +1,46 @@
-//import shopItens from '../../data/shop.json'
+import { FaShoppingCart, FaEye, FaShare, FaStarHalf, FaStar} from "react-icons/fa";
+
+import shopItens from '../../data/shop.json'
 import '../../styles/components/shop.sass'
 
 export default function Shop() {
     
     return (
-        <>
+        <section id="shop">
         
-        </>
+            <h2> Featured Products </h2>
+
+            <div className="shop-products">
+
+                {shopItens?.map((item, index) => (
+                    <div key={index} className="shop-item">
+                        
+                        <div className="image">
+                            <img src={item.image} alt="" />
+                            <div className="icons">
+                                <FaShoppingCart />
+                                <FaEye />
+                                <FaShare />
+                            </div>
+                        </div>
+                        
+                        <div className="content">
+                            <h3> Survival Kits </h3>
+                            <span> {item.price} </span>
+                            <div className="stars">
+                                <FaStar size={16}/>
+                                <FaStar size={16}/>
+                                <FaStar size={16}/>
+                                <FaStar size={16}/>
+                                <FaStarHalf size={16}/>
+                            </div>
+                        </div>
+                        
+                    </div>
+                ))}
+
+            </div>
+
+        </section>
     )
 }
