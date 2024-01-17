8e27e1b6a86c417b97cb1ad09f9307463b245087
Luis Gustavo Braga <luisgustavobsilva@gmail.com>
2024-01-17 16:06:45 -0300

    Criando e estilizando o componente 'Services'

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Category from './components/Category'
 import Header from './components/Header'
 import Home from './components/Home'
 import Package from './components/Packages'
+import Services from './components/Services'
 import Shop from './components/Shop'
 import './styles/styles.sass'
 
@@ -16,6 +17,7 @@ function App() {
           <About/>
           <Shop/>
           <Package/>
+          <Services/>
     </>
   )
 }
---
+++ src/components/Services/index.jsx
@@ -0,0 +1,26 @@
+import servicesItens from '../../data/services.json'
+import '../../styles/components/services.sass'
+
+export default function Services() {
+
+    return (
+        <section id="services">
+
+            <h1 className="heading"> What We Offer </h1>
+
+            <div className="box-container">
+
+                {servicesItens.map((item, index) => (
+                    <div className="box" key={index}>
+                        <img src={item.image} alt="" />
+                        <h3> {item.title} </h3>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis, reiciendis?</p>
+                        <a href="#"> Read More </a>
+                    </div>
+                ))}
+
+            </div>
+
+        </section>
+    )
+}
