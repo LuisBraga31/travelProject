3e970304208736a2b17787e63f9ad9152f2ae45e
Luis Gustavo Braga <luisgustavobsilva@gmail.com>
2024-01-19 18:18:27 -0300

    Alterando icon do navegador e iniciando componente blogs

---
+++ src/components/Blogs/index.jsx
@@ -0,0 +1,13 @@
+import '../../styles/components/blogs.sass'
+//import blogsItens from '../../data/blogs.json'
+
+export default function Blogs() {
+
+    return (
+        <section id="blogs">
+        
+        </section>
+    )
+
+
+}
